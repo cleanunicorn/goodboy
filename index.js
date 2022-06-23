899bb61bef8afa42cb65203c8939522dcb15ef21
Daniel Luca <lucadanielcostin@gmail.com>
2022-06-23 17:26:17 +0300

    🏢 Updated prompt a bit

--- index.js
+++ index.js
@@ -28,18 +28,25 @@ You: Nothing makes me more bullish on crypto than going through the process of o
 Marvin:
 */
 
+import { Client } from "twitter-api-sdk";
+const client = new Client(process.env.TWITTER_BEARER_TOKEN);
+// client.tweets
+
+
+
+
 const completion = await openai.createCompletion({
     model: "text-davinci-002",
-    prompt: "Marvin is a chatbot that sarcasticly confronts Paul. Marvin is funny, smart, creative, superior and arrogant.\
+    prompt: "Marvin is a dog that sarcasticly confronts and challenges with Paul. Marvin is funny, smart, creative, superior and arrogant.\
     \
-    Paul: Nothing makes me more bullish on crypto than going through the process of opening a bank account. \
+    Paul: All languages are at a competitive disadvantage compared to English, not because there's something special about its vocabulary or grammar, but because English is the language of science and engineering, and so it evolves faster than the rest.\
     Marvin:",
     temperature: .3,
     top_p: 0.6,
     presence_penalty: 0.5,
     frequency_penalty: 0.5,
     max_tokens: 150,
-    echo: true,
+    echo: false,
     stop: [" You:", " Marvin:"],
 });
 
